9f1b72e60ccf1df5ddbded1b2eb83ec115ffdf61
qkudev <qkud.ev@gmail.com>
2023-10-19 20:52:58 +0200

    store-datam, heap docs

--- src/event-emitter/__tests__/event-emitter.test.ts
+++ src/event-emitter/__tests__/event-emitter.test.ts
@@ -1,4 +1,4 @@
-import EventEmitter from '../EventEmitter';
+import EventEmitter from '../event-emitter';
 
 const event = 'test-event';
 
--- src/heap/heap.ts
+++ src/heap/heap.ts
@@ -1,12 +1,32 @@
 type Comparator<T> = (a: T, b: T) => number;
 
+/**
+ * A class representing a heap data structure.
+ * @template T The type of elements stored in the heap.
+ */
 class Heap<T> {
+  /**
+   * The internal state of the heap.
+   * @private
+   */
   private state: T[] = [null as any];
 
+  /**
+   * The real size of the heap.
+   * @private
+   */
   private realSize = 0;
 
+  /**
+   * Creates a new instance of the Heap class.
+   * @param compareFn A function used to compare elements in the heap.
+   */
   constructor(private readonly compareFn: Comparator<T>) {}
 
+  /**
+   * Adds an element to the heap.
+   * @param val The element to add to the heap.
+   */
   public push = (val: T) => {
     this.realSize++;
     this.state.push(val);
@@ -14,13 +34,21 @@ class Heap<T> {
     let index = this.state.length - 1;
     let parentIndex = Math.floor(index / 2);
 
-    while (index > 1 && parentIndex > 0 && this.compare(index, parentIndex) < 0) {
+    while (
+      index > 1 &&
+      parentIndex > 0 &&
+      this.compare(index, parentIndex) < 0
+    ) {
       this.swap(index, parentIndex);
       index = parentIndex;
       parentIndex = Math.floor(index / 2);
     }
   };
 
+  /**
+   * Removes and returns the smallest element from the heap.
+   * @returns The smallest element in the heap, or undefined if the heap is empty.
+   */
   public pop = (): T | undefined => {
     if (this.realSize === 0) {
       return undefined;
@@ -54,12 +82,26 @@ class Heap<T> {
     return returnValue;
   };
 
+  /**
+   * Returns the smallest element in the heap without removing it.
+   * @returns The smallest element in the heap, or undefined if the heap is empty.
+   */
   public peek = (): T | undefined => this.state[1];
 
+  /**
+   * Returns the number of elements in the heap.
+   */
   public get size(): number {
     return this.realSize;
   }
 
+  /**
+   * Compares two elements in the heap.
+   * @param i The index of the first element to compare.
+   * @param j The index of the second element to compare.
+   * @returns A negative number if the first element is smaller, a positive number if the second element is smaller, or zero if they are equal.
+   * @private
+   */
   private compare = (i: number, j: number) => {
     if (this.state[j] === undefined) {
       if (this.state[i] === undefined) {
@@ -74,6 +116,12 @@ class Heap<T> {
     return this.compareFn(this.state[i], this.state[j]);
   };
 
+  /**
+   * Swaps two elements in the heap.
+   * @param i The index of the first element to swap.
+   * @param j The index of the second element to swap.
+   * @private
+   */
   private swap = (i: number, j: number) => {
     const temp = this.state[i];
     this.state[i] = this.state[j];
