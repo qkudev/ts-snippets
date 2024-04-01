97b3c9cd3c2fb7ea0efd16f7399968afd9e8b514
qkudev <qkudev@gmail.com>
2024-04-01 09:07:19 +0200

    `merge`
    `merge(obj1, obj2)` returns new object with
    properties merged.
    
    ## Example
    ```
    const obj1 = {
      a: {
        b: 1,
        c: 3,
      }
    }
    conts obj2 = {
      a: {
        b: 2,
        d: 4
      }
    }
    console.log(merge(obj1, obj2)) // { a: { b: 2, c: 4, d: 4 }}
    ```

---
+++ src/merge/__tests__/merge.test.ts
@@ -0,0 +1,65 @@
+import merge from '../merge';
+
+describe('merge', () => {
+  it('should merge two plain objects', () => {
+    const obj1 = { a: 1 };
+    const obj2 = { b: 2 };
+
+    const result = merge(obj1, obj2);
+
+    expect(result).toEqual({ a: 1, b: 2 });
+  });
+
+  it('should merge two nested objects', () => {
+    const obj1 = { a: 1, b: { c: 2 } };
+    const obj2 = {
+      foo: {
+        bar: 'buzz',
+      },
+    };
+
+    const result = merge(obj1, obj2);
+
+    expect(result).toEqual({
+      a: 1,
+      b: {
+        c: 2,
+      },
+      foo: {
+        bar: 'buzz',
+      },
+    });
+  });
+
+  it('should overwrite property', () => {
+    const obj1 = {
+      a: {
+        b: 2,
+      },
+    };
+    const obj2 = {
+      a: {
+        b: 3,
+        c: 4,
+      },
+    };
+
+    const result = merge(obj1, obj2);
+
+    expect(result).toEqual({
+      a: {
+        b: 3,
+        c: 4,
+      },
+    });
+  });
+
+  it('should handle null arg', () => {
+    const obj1 = null;
+    const obj2 = { a: 1 };
+
+    const result = merge(obj1, obj2);
+
+    expect(result).toEqual({ a: 1 });
+  });
+});
